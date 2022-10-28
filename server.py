42665cffab65d94bc82488bb5f13b8fe8a61a19a
Rutvik <rutvikachgunde@gmail.com>
2022-10-28 12:52:06 +0530

    add files

---
+++ server.py
@@ -0,0 +1,40 @@
+from email.quoprimime import quote
+from lib2to3.pgen2.token import NEWLINE
+from urllib import request
+from flask import Flask, render_template,url_for,request,redirect
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt',mode='a') as database:
+        email=data["email"]
+        subject=data["subject"]
+        message=data["message"]
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database2:
+        email=data["email"]
+        subject=data["subject"]
+        message=data["message"]
+        csv_writer=csv.writer(database2 , delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+@app.route('/sumbit_form',methods=['POST','GET'])
+def sumbit_form():
+    if request.method == 'POST':
+        data=request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return'Somehing went wrong'
+
+if __name__ == '__main__':
+    app.run(debug=True) 
